ab4ef19e3c0c6d81070feaba890825d41acd1d9b
Barajas <hbarajas@FVFZMYB5J1WK.tld>
2022-03-28 12:46:43 -0500

    third commit

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ function askTempature() {
     return temp
 }
 
+askTempature()
 
 function ctofConversion(f) {
     var tempcf = ((temp * 9)/5)+32; 
@@ -31,24 +32,26 @@ function ftocConversion(c) {
 
 
 function askConversion() {
-    var convert = prompt("If you want to convert from celsius to farenheit click f \n If you want to convert from farenheit to celsius click c")
-    return convert
+    var convertask = prompt("If you want to convert from celsius to farenheit click 2 \n If you want to convert from farenheit to celsius click 1")
+    return convertask
 }
 
 askConversion()
 
-function convert( tempcf, tempfc ) { // check parameters // add placeholders
-    if (convert == "f" ) {
-        alert(`${askTempature()} celsius is ${ftocConversion()} farenheit.`)
+var convertques = (askConversion)
+
+function convertAnswer( askTempature, ftocConversion, ctofConversion) { // check parameters // add placeholders
+    if (convertques == 2) {
+        alert(`${askTempature()} celsius is ${ctofConversion()} farenheit.`)
     }
-    else if (convert == "c") {
-        alert(`${askTempature()} farenheit is ${ctofConversion()} celsius.`)
+    else if (convertques == 1) {
+        alert(`${askTempature()} farenheit is ${ftocConversion()} celsius.`)
     }
-    return convert
 }
 
-convert()
+convertAnswer()
 
 // ((tempcf * 9)/5)+32
 
 // ((tempfc - 32)* 5)/9
+
